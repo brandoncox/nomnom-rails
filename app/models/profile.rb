e64699be681089274eab829162583c04955dab18
Brandon Cox <brandon.cox.519@gmail.com>
2013-07-21 01:07:21 -0400

    added new fields for chat protocols

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,11 @@
 class Profile < Entity
   embedded_in :user
   
+  CHAT_TYPES = ['AIM', 'Google Talk', 'Windows Live Messenger','Skype', 'Yahoo! Messenger',
+               'Gadu-Gadu', 'ICQ','QQ','NateOn','Twitter', 'Hyves', 'Orkut',
+               'Cyworld','QIP','Rediff Bol', 'Vkontakte','eBuddy','Mail.ru Agent',
+               'Jabber','BlackBerry Messenger']
+  
   field :first_name, type: String
   field :last_name, type: String
   field :address_1, type: String
@@ -9,5 +14,7 @@ class Profile < Entity
   field :state, type: String
   field :zip, type: String
   field :phone_number, type: String
+  field :chat_type, type: String
+  field :screen_name, type: String
 
 end
