4a3d1add72a71f172de304e255a41d60b188c508
Brandon Cox <brandon.cox.519@gmail.com>
2013-07-21 01:07:48 -0400

    additional changes for the profile setup wizard

--- app/controllers/profile_steps_controller.rb
+++ app/controllers/profile_steps_controller.rb
@@ -1,9 +1,19 @@
 class ProfileStepsController < ApplicationController
   include Wicked::Wizard
-  steps :basic, :contact, :social
+  steps :basic, :contact, :social, presence: true
+  
+  def new
+    @profile = Profile.new
+  end
   
   def show
-    @user = current_user
+    current_user.profile = Profile.new
+    @profile = current_user.profile
     render_wizard 
+  end
+  
+  def update
+    @profile = current_user.create_profile(params[:profile])
+    render_wizard @profile
   end
 end
