6564a8ae5932ee286883c6e31c8019f1bba2bd41
Brandon Cox <brandon.cox.519@gmail.com>
2013-07-21 23:53:52 -0400

    small changes

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,10 +1,12 @@
 class Profile < Entity
   embedded_in :user
+  cattr_accessor :messager_types, :contact_methods
   
-  CHAT_TYPES = ['AIM', 'Google Talk', 'Windows Live Messenger','Skype', 'Yahoo! Messenger',
-               'Gadu-Gadu', 'ICQ','QQ','NateOn','Twitter', 'Hyves', 'Orkut',
-               'Cyworld','QIP','Rediff Bol', 'Vkontakte','eBuddy','Mail.ru Agent',
-               'Jabber','BlackBerry Messenger']
+  @@messager_types = [['',0],['AIM',1],['Google Talk',2], ['Windows Live Messenger',3],['Skype',4], ['Yahoo! Messenger',5],
+               ['Gadu-Gadu',6], ['ICQ',7],['QQ',8],['NateOn',9],['Twitter',10], ['Hyves',11], ['Orkut',12],
+               ['Cyworld',13],['QIP',14],['Rediff Bol',15], ['Vkontakte',16],['eBuddy',17],['Mail.ru Agent',18],
+               ['Jabber',19],['BlackBerry Messenger',20]]
+  @@contact_methods = [:sms, :email, :phone, :messager]
   
   field :first_name, type: String
   field :last_name, type: String
@@ -14,7 +16,9 @@ class Profile < Entity
   field :state, type: String
   field :zip, type: String
   field :phone_number, type: String
-  field :chat_type, type: String
-  field :screen_name, type: String
+  # messenger should be an array of key values of messenger type to screename and a mapping for preferred screename
+  # exampe entry should be {:aim => 'my_aim_screename, :preferred => true}
+  field :messenger, type: Array
+  field :preffered_contact_method, type: String
 
 end
